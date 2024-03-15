4dda77c296310071ccbbf90ef4a6976858461226
fiza _nawaz <162031014+fizza-nawaz@users.noreply.github.com>
2024-03-15 20:38:46 +0500

    Add files via upload
    Add an optional extanded description

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Assidment # 1
+// storing the person name in a variabl
+let personName = "Fiza";
+// printing a message to the person 
+console.log(`Hello ${personName}, would you like to learn some paython today?`);
---
+++ main.ts
@@ -0,0 +1,9 @@
+// Assidment # 1
+
+// storing the person name in a variabl
+
+let personName:string ="Fiza";
+
+// printing a message to the person 
+
+console.log(`Hello ${personName}, would you like to learn some paython today?`)
